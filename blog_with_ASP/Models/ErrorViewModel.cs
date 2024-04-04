cedcceeec6ac15b0bc619b06dbdcf5c035af9b85
Hqko01 <hakan.yilmaz0134@hotmail.com>
2024-04-04 16:09:12 +0300

    first-upload

---
+++ blog_with_ASP/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace blogWithASP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
